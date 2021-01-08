0331a01516fa2797a07c111a39b7a9c77f4c00f8
Amanda Barr <amanda.j.webster86@gmail.com>
2021-01-08 14:51:10 -0800

    Initial variables in battleship.js
    Created initial variables in battleship.js. Currently, the battleship is located at a set location.

---
+++ battleship.js
@@ -0,0 +1,21 @@
+// Three variables to hold the location of each part of the battlship (battleship is three cells long)
+
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+// Variable to hold the user's current guess
+
+var guess;
+
+// Variable to hold the number of hits
+
+var hits = 0;
+
+// Varible to hold the number of guesses
+
+var guesses = 0;
+
+// Variable to keep track of whether or not the ship is sunk
+
+var isSunk = false;
