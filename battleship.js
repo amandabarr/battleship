cb5f2d112d33474b4799a26e6da2227d7a2d79e4
Amanda Barr <amanda.j.webster86@gmail.com>
2021-01-08 15:26:24 -0800

    Update battleship.js
    Added a check of the user's input to make sure the guess is within range of the playing board.
    
    Added code to detect if ship was hit and if so it ship had sunk.
    
    Added stats variable and alert to notify user of shooting accuracy.

--- battleship.js
+++ battleship.js
@@ -20,6 +20,31 @@ var guesses = 0;
 
 var isSunk = false;
 
+
 while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a number from 0 to 6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("HIT!");
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS!");
+        }
+    }
 }
+
+
+// Variable to let user know how many guesses it took to sink the battleship
+
+var stats = "You took " + guesses + " guesses to sink the battleship, which means your shooting accuracy was " + (3/guesses);
+
+alert(stats);
